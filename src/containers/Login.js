4c9fb4fcb39e20fa52beabed85e477b89216dbfc
Diego <Diegoarod14@gmail.com>
2023-09-06 22:05:57 -0400

    Commented dispatch for now

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -5,8 +5,8 @@ import {useDispatch, useSelector} from 'react-redux';
 
 export const Login = ()=>{
 
-    const dispatch = useDispatch()
-    const data = useSelector(state =>state.reducerName.property)
+    // const dispatch = useDispatch()
+    // const data = useSelector(state =>state.reducerName.property)
     return (
         <div>Login</div>
     )
